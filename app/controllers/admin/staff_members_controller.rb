3fbb742e8db223ec4d896af16c323ff2d53ce535
Rairiki Volt <rairikivolt@hotmail.com>
2015-06-13 18:30:49 +0900

    11-1 前半
    index, show, new, edit実装

---
+++ app/controllers/admin/staff_members_controller.rb
@@ -0,0 +1,18 @@
+class Admin::StaffMembersController < Admin::Base
+  def index
+    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+  end
+
+  def show
+    @staff_member = StaffMember.find(params[:id])
+    redirect_to [:edit, :admin, staff_member]
+  end
+
+  def new
+    @staff_member = StaffMember.new #staff_memEber -> staff_member :p
+  end
+
+  def edit
+    @staff_member = StaffMember.find(params[:id])
+  end
+end
