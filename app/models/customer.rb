1c70403fbbea8e7bdd17a89dfcc26f36befc2158
Rairiki Volt <rairikivolt@hotmail.com>
2015-07-08 02:09:42 +0900

    19-3 ActiveSupport::Concernによるコード共有

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,10 @@
 class Customer < ActiveRecord::Base
+  include EmailHolder
+  include PersonalNameHolder
+  include PasswordHolder
+
   has_one :home_address, dependent: :destroy, autosave: true #destroy > :destroy
   has_one :work_address, dependent: :destroy, autosave: true
-
-  before_validation do
-    self.email_for_index = email.downcase if email
-  end
 
   validates :gender, inclusion: { in: %w(male female), allow_blank: true }
   validates :birthday, date: {
@@ -12,12 +12,4 @@ class Customer < ActiveRecord::Base
     before: -> (obj) { Date.today },
     allow_blank: true
   }
-
-  def password=(raw_password) # passsword > password
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?  # else > elsif
-      self.hashed_password = nil
-    end
-  end
 end
