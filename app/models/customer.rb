704e0fc5987ed27b590b871b482e9f055c925068
Rairiki Volt <rairikivolt@hotmail.com>
2015-07-08 01:08:41 +0900

    19-2 顧客アカウント新規登録／更新機能の改良

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,17 @@
 class Customer < ActiveRecord::Base
-  has_one :home_address, dependent: :destroy #destroy > :destroy
-  has_one :work_address, dependent: :destroy
+  has_one :home_address, dependent: :destroy, autosave: true #destroy > :destroy
+  has_one :work_address, dependent: :destroy, autosave: true
 
   before_validation do
     self.email_for_index = email.downcase if email
   end
+
+  validates :gender, inclusion: { in: %w(male female), allow_blank: true }
+  validates :birthday, date: {
+    after: Date.new(1900, 1, 1),
+    before: -> (obj) { Date.today },
+    allow_blank: true
+  }
 
   def password=(raw_password) # passsword > password
     if raw_password.kind_of?(String)
